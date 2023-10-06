4ffb717d0df5fa80b62fd544e3ea0e0e8bc5b86e
Benjamin Appiah <ben.appiah451@gmail.com>
2023-10-05 19:18:35 -0700

    almost done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
-#!/bin/bash
+#include <stdio.h>
+
+/*prints out exactly what the text says with an open quotes*/
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n")
+	return(0)
 }
