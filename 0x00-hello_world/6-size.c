0048aabd4e6d8fa75dedd1665b9f1db32627999b
Benjamin Appiah <ben.appiah451@gmail.com>
2023-10-05 20:05:26 -0700

    sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Return: always return 0
+ * */
+
+int main(void)
+{
+	printf("Size of char: %c byte(s)\n, sizeof(char)");
+	printf("Size of int: %i byte(s)\n, sizeof(int)");
+	printf("Size of long int: %lu byte(s)\n, sizeof(long int)");
+	printf("Size of long long int: %llu by te(s)\n, sizeof(long long int)");
+	printf("Size of float: %f byte(s)\n, sizeof(float)");
+	return (0);
+}
