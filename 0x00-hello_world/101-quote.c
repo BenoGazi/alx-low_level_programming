7cb54942599d1ee20fcf53aa27dd61756cde388f
Benjamin Appiah <ben.appiah451@gmail.com>
2023-10-06 05:11:48 -0700

    phew....good peers are always the solution

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  *main - main function
  *
  *Return: always return 1
 */
+
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",61);
 	return (1);
 }
