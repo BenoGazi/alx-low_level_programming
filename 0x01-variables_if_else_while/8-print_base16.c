c2aa52d40ab9e9810adf9134d69bb0104e44f2a3
Benjamin Appiah <ben.appiah451@gmail.com>
2023-10-07 06:59:59 -0700

    print all base 16 digits in lowercase letters

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - main function: to print all numbers of base16 in lowercase
+ *
+ * Return: always return 0
+ *
+ */
+
+int main(void)
+{
+	int i;
+	char j;
+
+	for (i = 0; i <= 10; i++)
+	{
+		putchar((i % 10) + '0');
+	}
+	for (j = 'a'; j = 'f'; j++)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+	return (0);
+}
