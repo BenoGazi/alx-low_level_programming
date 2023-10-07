9b50f90cc8affafad9d5944af54c912a291102fe
Benjamin Appiah <ben.appiah451@gmail.com>
2023-10-06 19:22:03 -0700

    we'll talk about 10 later

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/*
+ * function  main - function prints numbers of base 10 starting with 0
+ *
+ * Return: always return 0
+ *
+ * main: main function
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i\n", i);
+	}
+}
