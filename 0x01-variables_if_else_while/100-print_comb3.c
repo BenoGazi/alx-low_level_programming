f15cf369f7132250c2fb601247060c39b1ddf8ef
Benjamin Appiah <ben.appiah451@gmail.com>
2023-10-07 09:42:19 -0700

    lets combine two numbers now

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - prints all possible combinations of two digit numbers
+ *
+ * Return: always return 0
+ *
+ */
+
+int main(void)
+{
+	int i, j;
+	
+
+	for (i = 0; i < 10; i++)
+	{
+		for (i = j; j < 10; j++)
+		{
+			putchar((i % 10) + '0');
+			putchar((j % 10) + '0');
+
+			if (i == 8 && j == 9)
+			{
+				continue;
+			}
+			putchar(',');
+			putchar(' ');
+		}
+	}
+		putchar('\n');
+		return (0);
+
+	
+}
