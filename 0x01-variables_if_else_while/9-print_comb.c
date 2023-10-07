c75f770f92008a2f17fd10c7e2ee4659fa739909
Benjamin Appiah <ben.appiah451@gmail.com>
2023-10-07 08:00:34 -0700

    all days

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - main function prints all possible combination of single digit numbers
+ *
+ * Return: always returns 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar((i % 10) + '0');
+		if (i == 9)
+		{
+			continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
