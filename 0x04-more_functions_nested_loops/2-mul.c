ec680f8f8869f0cfe821f6814771675538a2707d
Benjamin Appiah <ben.appiah451@gmail.com>
2023-10-12 05:55:38 -0700

    lets multiple two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ *
+ * @a: first interger to check
+ * @b: second integer to be checked
+ *
+ * Return: Always return the result
+ */
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
