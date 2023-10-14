b2e36905de0031f995baff2deb4e2ab2a5a0d717
Benjamin Appiah <ben.appiah451@gmail.com>
2023-10-14 10:27:21 -0700

    finish it up

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,7 +16,7 @@ void jack_bauer(void)
 	{
 	for (l = 0; l <= 9; l++)
 	{
-	if ((i <= 1 && j <=9) || (i <=2 && j <= 3))
+	if ((i <= 1 && j <= 9) || (i <= 2 && j <= 3))
 	{
 		_putchar(i + '0');
 		_putchar(j + '0');
@@ -26,7 +26,7 @@ void jack_bauer(void)
 		_putchar('\n');
 	}
 	}
-	}	
+	}
 	}
 	}
 }
