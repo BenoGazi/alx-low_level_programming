facc3f4514a3a79ec425247b5535cd677117a9da
Benjamin Appiah <ben.appiah451@gmail.com>
2023-10-16 15:27:02 -0700

    print elements of the array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_array - prints elemsts of arrays
+ *
+ * @a: input
+ * @n: length of array
+ *
+ * return: void
+ *
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+	if (i != (n - 1))
+	{
+		printf(",");
+	}
+	}
+	printf("\n");
+}
