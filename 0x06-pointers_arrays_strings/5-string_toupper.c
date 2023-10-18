b949be8e1af0df68adbaa63842b0c7f3cacce8b8
Benjamin Appiah <ben.appiah451@gmail.com>
2023-10-18 09:20:45 -0700

    convert all lowercase letters to uppercae

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters to uppercase
+ *
+ * @upper: parameter
+ *
+ * Return: string of uppercase words
+ */
+
+char *string_toupper(char *upper)
+{
+	int i = 0;
+
+	while (upper[i] != '\0')
+	{
+		if (upper[i] >= 'a' && upper[i] <= 'z')
+		{
+			upper[i] = upper[i] - 32;
+		}
+		i++;
+	}
+	return(upper);
+}
