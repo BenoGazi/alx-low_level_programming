f0a5c4fd64f6321e656c6f7ab849cf51dc7efbe7
Benjamin Appiah <ben.appiah451@gmail.com>
2023-10-24 20:24:25 -0700

    sets a pointer to char

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * set_string - main function
+ *
+ * @s: pointer param
+ * @to: parameter
+ *
+ * Return: void
+ *
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
