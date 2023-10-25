40139ccee6e49e37bf5412bac6d613240839ede5
Benjamin Appiah <ben.appiah451@gmail.com>
2023-10-24 18:18:23 -0700

    searches for substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strstr - locates a substring
+ *
+ * @haystack: string
+ * @needle: substring to search
+ *
+ * Return: Always 0
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+	int j;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i + j] != needle[j])
+			{
+				break;
+			}
+			else if (needle[j] != '\0')
+			{
+				return (&haystack[i]);
+			}
+		}
+	}
+	return (0);
+}
