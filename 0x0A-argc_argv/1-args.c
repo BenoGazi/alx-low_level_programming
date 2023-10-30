1162ef12a0e41bbb3dccbd4dc292f636f7e7a986
Benjamin Appiah <ben.appiah451@gmail.com>
2023-10-29 17:59:01 -0700

    let's print numbers

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%i\n", argc);
+	return (0);
+}
