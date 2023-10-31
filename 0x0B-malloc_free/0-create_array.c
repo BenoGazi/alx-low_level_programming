34e5031150776d671093031ee78d6bf3888585a1
Benjamin Appiah <ben.appiah451@gmail.com>
2023-10-31 04:13:56 -0700

    corrections made

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,7 +16,16 @@ char *create_array(unsigned int size, char c)
 	char *arr;
 	unsigned int i;
 
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
 	arr = malloc(sizeof(char) * size);
+	if (arr == 0)
+	{
+		return (NULL);
+	}
 	for (i = 0; i < size; i++)
 	{
 		arr[i] = c;
