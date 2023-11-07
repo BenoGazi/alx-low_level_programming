9fdd88777a5b25a9ee2d772e5bbae51a67b7cab6
Benjamin Appiah <ben.appiah451@gmail.com>
2023-11-07 03:15:54 -0800

    structures in c first dog.h file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_HEADER_FILE
+#define DOG_HEADER_FILE
+
+/**struct dog - dog struct data type
+ * @name: member variable
+ * @age: member variable
+ * @owner: member variable
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
