7be7cb9e6b405918d8773ea8521de2af117362bb
Benjamin Appiah <ben.appiah451@gmail.com>
2023-11-07 03:55:06 -0800

    dog initialisation

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -7,14 +7,15 @@
  * @name: param
  * @age: param
  * @owner: param
+ * 
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	do
+	if (d != NULL)
 	{
 		d->name = name;
 		d->age = age;
 		d->owner = owner;
-	}while(d != NULL);
+	}
 }
