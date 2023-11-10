4c247b6bf5daea0eb6a8572a3aa906149f5d26f8
Benjamin Appiah <ben.appiah451@gmail.com>
2023-11-09 21:16:54 -0800

    print all

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_name - prints names
+ * @f: param
+ * @name: name parameter
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+	{
+		while (*name != '\0')
+		{
+			f(name);
+			name++;
+		}
+	}
+}
