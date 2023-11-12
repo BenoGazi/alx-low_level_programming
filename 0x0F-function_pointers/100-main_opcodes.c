b1a4b5424e6f9d0ce8a1b23f74ea2bff46aa451d
Benjamin Appiah <ben.appiah451@gmail.com>
2023-11-12 12:50:01 -0800

    opcodes

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * main - main function
+ * @argc: argument count
+ * @argv: argument array
+ * Return: operation code
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j;
+	char *args;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	j = atoi(argv[1]);
+	if (j < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	args = (char *)main;
+	i = 0;
+	while (args[i] != '\0')
+	{
+		printf("%02hhx", args[i]);
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
