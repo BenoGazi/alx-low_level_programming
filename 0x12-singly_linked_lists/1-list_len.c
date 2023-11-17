2eafe11e758fdcd96682010d54c6a48a7ca127bb
Benjamin Appiah <ben.appiah451@gmail.com>
2023-11-16 17:01:51 -0800

    list length

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**list_len - prints the number of elements
+ * @h: pointer parameter
+ * Return: success
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t node = 0;
+
+	while (h != NULL)
+	{
+		node++;
+		h = h->next;
+	}
+	return (node);
+}
