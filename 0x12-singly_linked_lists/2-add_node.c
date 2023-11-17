f8a67c569171849c7a6bb866663260c53d3dbde5
Benjamin Appiah <ben.appiah451@gmail.com>
2023-11-16 17:23:33 -0800

    hmm

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -2,8 +2,8 @@
 
 /**
  * add_node - adds a new node
- * @str:
- * @head:
+ * @str: string param
+ * @head: new node
  * Return: node
  */
 
@@ -28,7 +28,7 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	}
 	new_node->str = strdup(str);
-	if(new_node->str == NULL)
+	if (new_node->str == NULL)
 	{
 		free(new_node);
 		return (NULL);
