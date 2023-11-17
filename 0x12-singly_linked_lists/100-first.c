b285864349b75ef27676802b0c8d197d4a9836cd
Benjamin Appiah <ben.appiah451@gmail.com>
2023-11-16 18:14:20 -0800

    before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+ * before - prints a sentence before main executes
+ */
+
+void _before(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
