cac417446b0f9bc146383749f2dff1dca9a21a0c
Benjamin Appiah <ben.appiah451@gmail.com>
2023-11-16 16:37:28 -0800

    gosh

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * print_list - list to print
+ * @h: pointer parameter
+ * Return: Success
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t node = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0](nil)\n");
+		}
+		else
+		{
+			printf("[%u] %s\n", h->len, h->str);
+			node++;
+		}
+		h = h->next;
+	}
+	return (node);
+}
