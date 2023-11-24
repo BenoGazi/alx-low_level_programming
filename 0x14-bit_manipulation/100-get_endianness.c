5a0a1497c565054ccbdc091845ffc472b72779a7
Benjamin Appiah <ben.appiah451@gmail.com>
2023-11-24 07:06:48 -0800

    Little Endian, who are you and w\hy did you stress me

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks the endianness
+ * Return: Success
+ */
+
+int get_endianness(void)
+{
+	int i;
+	char *j;
+
+	i = 1;
+	j = (char *)&i;
+	return (*j == 1) ? 1 : 0;
+}
