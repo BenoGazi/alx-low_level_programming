df76293873033b50ba73f8d20cd6684bb44fa3e6
Benjamin Appiah <ben.appiah451@gmail.com>
2023-11-27 17:56:38 -0800

    lets crate files now

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: file to be created
+ * @text_context: Null terminated string
+ * Return: 1 on success and -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fp, i;
+	int str_len;
+
+	if (!filename)
+	{
+		return (-1);
+	}
+	if (text_content != NULL)
+	{
+		str_len = 0;
+		while (text_content[str_len])
+		{
+			str_len++;
+		}
+		fp = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+		if (fp == -1)
+		{
+			return (-1);
+		}
+		i = write(fp, text_content, str_len);
+		if (i == -1)
+		{
+			return (-1);
+		}
+		close(fp);
+	}
+	return (1);
+}
