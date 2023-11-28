b56cdc93348c9b0835457274351a050cbbac3341
Benjamin Appiah <ben.appiah451@gmail.com>
2023-11-27 18:04:18 -0800

    lets create agin

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,24 +16,21 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	if (text_content != NULL)
+	str_len = 0;
+	while (text_content[str_len])
 	{
-		str_len = 0;
-		while (text_content[str_len])
-		{
-			str_len++;
-		}
-		fp = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-		if (fp == -1)
-		{
-			return (-1);
-		}
-		i = write(fp, text_content, str_len);
-		if (i == -1)
-		{
-			return (-1);
-		}
-		close(fp);
+		str_len++;
 	}
+	fp = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (fp == -1)
+	{
+		return (-1);
+	}
+	i = write(fp, text_content, str_len);
+	if (i == -1)
+	{
+		return (-1);
+	}
+	close(fp);
 	return (1);
 }
