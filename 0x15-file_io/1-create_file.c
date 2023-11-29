e122f01e071ac57ea5354246f88dcbd833aeb624
Benjamin Appiah <ben.appiah451@gmail.com>
2023-11-29 07:54:43 -0800

    Let's try agin

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,19 +17,22 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 	str_len = 0;
-	while (text_content[str_len])
+	if (text_content)
 	{
-		str_len++;
-	}
-	fp = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	if (fp == -1)
-	{
-		return (-1);
-	}
-	i = write(fp, text_content, str_len);
-	if (i == -1)
-	{
-		return (-1);
+		while (text_content[str_len])
+		{
+			str_len++;
+		}
+		fp = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+		if (fp == -1)
+		{
+			return (-1);
+		}
+		i = write(fp, text_content, str_len);
+		if (i == -1)
+		{
+			return (-1);
+		}
 	}
 	close(fp);
 	return (1);
