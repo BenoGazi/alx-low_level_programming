55b26ca6a59f0740736938502718d38be9cac878
Benjamin Appiah <Shell Script to run Python script>
2024-01-11 15:39:40 -0800

    Sum of all data in the list

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - Sums all of the data in the list
+ * @head: Head node
+ * Return: Sum of  all of the data of a doubly linked list
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
