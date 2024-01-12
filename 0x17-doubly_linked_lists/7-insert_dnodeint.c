602416d1cc36c42b5281f95d4da7ae61d30aea2c
Benjamin Appiah <Shell Script to run Python script>
2024-01-11 19:00:15 -0800

    Lets try again

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -26,7 +26,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		{
 			if (i == idx)
 			{
-				if (!head->next)
+				if (head->next == NULL)
 					new_node = add_dnodeint(h, n);
 				else
 				{
