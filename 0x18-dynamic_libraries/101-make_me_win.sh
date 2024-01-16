ae3ba8257fb679c6f5131897a966289a8d0db3f9
Benjamin Appiah <Shell Script to run Python script>
2024-01-16 08:14:35 -0800

    Let's finish it up

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://raw.githubusercontent.com/BenoGazi/alx-low_level_programming/main/0x18-dynamic_libraries/lib101-makemewin.so
-export LD_PRELOAD="$PWD/../lib101-makemewin.so"
+wget -P .. https://raw.githubusercontent.com/BenoGazi/alx-low_level_programming/main/0x18-dynamic_libraries/libmytest.so
+export LD_PRELOAD="$PWD/../libmytest.so"
