c0b1e4b3c07c0688af3f16fee326b7c4598107dd
Benjamin Appiah <Shell Script to run Python script>
2024-02-03 03:16:32 -0800

    Key Index file added

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - Retirieve the index of a key/value pair in the array
+ * @key: The key of the index
+ * @size: The Array Sixe
+ * Return: The index at which the key/value pair should be stored
+ * int the array of the hash table
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
+{
+	return (hash_djb2(key) %  size);
+}
